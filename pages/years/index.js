860260060fbd296f44f954ae33717afe6684b378
Lachlan Campbell <lachlan@hackclub.com>
2019-12-23 23:48:27 -0500

    Add /years page

---
+++ pages/years/index.js
@@ -0,0 +1,12 @@
+import { Container, Box } from '@theme-ui/components'
+import Header from '../../components/header'
+import Years from '../../components/years'
+
+export default () => (
+  <Box as="main" sx={{ bg: 'background', color: 'text', textAlign: 'center' }}>
+    <Header title="Explore High School Hackathons by Year" includeMeta />
+    <Container sx={{ mt: [3, 4, 5], py: [3, null, 4] }}>
+      <Years />
+    </Container>
+  </Box>
+)
