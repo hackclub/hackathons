08aefe91b3ca10a0115b89bac832379e6485a031
Lachlan Campbell <lachlan@hackclub.com>
2020-01-15 15:45:09 -0500

    Add back button to nav bar

--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,14 @@
+import { ArrowLeft, Plus, Moon, GitHub } from 'react-feather'
+import {
+  Box,
+  Container,
+  IconButton,
+  Image,
+  Link as A
+} from '@theme-ui/components'
 import { useColorMode } from 'theme-ui'
-import { Box, Container, IconButton, Image, Link } from '@theme-ui/components'
-import { Plus, Moon, GitHub } from 'react-feather'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const NavButton = ({ sx, ...props }) => (
   <IconButton
@@ -18,13 +26,37 @@ const NavButton = ({ sx, ...props }) => (
   />
 )
 
+const BackButton = ({ to = '/' }) => (
+  <Link href={to} passHref>
+    <NavButton as="a" title={to === '/' ? 'Back to homepage' : 'Back'}>
+      <ArrowLeft />
+    </NavButton>
+  </Link>
+)
+
+const Flag = () => (
+  <A
+    href="https://hackclub.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label="Hack Club homepage"
+    sx={{ mt: -3 }}
+  >
+    <Image
+      src="https://hackclub.com/orpheus_flag.svg"
+      alt="Hack Club flag"
+      sx={{ width: [96, 128] }}
+    />
+  </A>
+)
+
 const ColorSwitcher = props => {
   const [mode, setMode] = useColorMode()
   return (
     <NavButton
       {...props}
       onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
-      title="Cycle Color Mode"
+      title="Reverse color scheme"
     >
       <Moon size={24} />
     </NavButton>
@@ -33,6 +65,8 @@ const ColorSwitcher = props => {
 
 export default () => {
   const [mode] = useColorMode()
+  const router = useRouter()
+  const home = router.pathname === '/'
   return (
     <Box
       as="nav"
@@ -55,19 +89,7 @@ export default () => {
           }
         }}
       >
-        <Link
-          href="https://hackclub.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Hack Club homepage"
-          sx={{ mt: -3 }}
-        >
-          <Image
-            src="https://hackclub.com/orpheus_flag.svg"
-            alt="Hack Club flag"
-            sx={{ width: [96, 128] }}
-          />
-        </Link>
+        {!home ? <BackButton /> : <Flag />}
         <NavButton
           as="a"
           href="https://airtable.com/shr42MplImeMkHHWP"
