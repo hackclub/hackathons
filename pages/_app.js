ebb3674a2ffb7adf1c7c7d6b2637a1d390f389aa
Lachlan Campbell <lachlan@hackclub.com>
2020-01-15 15:19:13 -0500

    Revert "Add Segment analytics"
    This reverts commit 9c5ed023a1d395021ba2230b8448ae4495fecbb5.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 import App from 'next/app'
 import { ThemeProvider, ColorMode } from 'theme-ui'
 import { Global } from '@emotion/core'
@@ -16,13 +15,7 @@ class Root extends App {
     return (
       <>
         <Meta />
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0"; analytics.load("enReVnqn2tVrMigdaSA5py2tRjSzlgHb"); analytics.page(); }}();`
-            }}
-          />
-          <style>{`
+        <style>{`
           @font-face {
             font-family: 'Phantom Sans';
             src: url('https://hackclub.com/fonts/Phantom_Sans_0.4/Regular.woff')
@@ -44,7 +37,6 @@ class Root extends App {
             font-display: swap;
           }
         `}</style>
-        </Head>
         <ThemeProvider theme={theme}>
           <ColorMode />
           <Global styles={theme => ({ body: theme.styles.root })} />
