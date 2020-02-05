717a45d3e9b807b536f866e08421ead88f961b0b
Lachlan Campbell <lachlan@hackclub.com>
2020-02-05 14:57:45 -0500

    Switch to Hack Club Theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import App from 'next/app'
+import Head from 'next/head'
+import Meta from '@hackclub/meta'
 import { ThemeProvider } from 'theme-ui'
 
+import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '../components/theme'
-import Meta from '../components/meta'
 import NProgress from '../components/nprogress'
 import Nav from '../components/nav'
 import Footer from '../components/footer'
@@ -13,7 +15,15 @@ export default class extends App {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
-        <Meta />
+        <Head>
+          <Meta
+            title="Hack Club Hackathons"
+            name="Hack Club Hackathons"
+            description="Listing of upcoming high school hackathons around the world."
+            image="https://hackathons.hackclub.com/card.png"
+          />
+          <title>Hack Club Hackathons</title>
+        </Head>
         <NProgress color={theme.colors.primary} />
         <Nav />
         <Component {...pageProps} />
