5a5c3613ac0131ef2bdbb2208c65b3548d175558
Lachlan Campbell <lachlan@hackclub.com>
2020-04-21 22:27:16 -0400

    Improve SEO on year pages

--- pages/years/[year].js
+++ pages/years/[year].js
@@ -7,7 +7,13 @@ import { getEvents } from '../../lib/data'
 export default ({ year, events }) => {
   if (!year || !events) return <Error statusCode={404} />
   return (
-    <Grouping title={`${year} Events`} events={events}>
+    <Grouping
+      title={`${year} High School Hackathons`}
+      desc={`Browse all ${events.length} hackathons for high schoolers ${
+        new Date().getFullYear().toString() === year ? 'in' : 'from'
+      } ${year}.`}
+      events={events}
+    >
       <Years showAll />
     </Grouping>
   )
@@ -16,16 +22,16 @@ export default ({ year, events }) => {
 export const getStaticPaths = async () => {
   let events = await getEvents()
   let starts = map(events, 'start')
-  starts = map(starts, start => first(split(start, '-')))
+  starts = map(starts, (start) => first(split(start, '-')))
   let years = uniq(starts)
-  const paths = map(years, year => ({ params: { year } }))
+  const paths = map(years, (year) => ({ params: { year } }))
   return { paths, fallback: false }
 }
 
 export const getStaticProps = async ({ params }) => {
   const { year } = params
   let events = await getEvents()
-  events = filter(events, e => startsWith(e.start, year))
+  events = filter(events, (e) => startsWith(e.start, year))
   events = orderBy(events, 'start')
   return { props: { year, events } }
 }
