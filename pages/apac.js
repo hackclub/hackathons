6bcb01bdbc7d83535d43fa98e48cf381e0ad604c
eilla1 <git@ella.cx>
2021-11-04 13:34:30 -0700

    Make page work

--- pages/apac.js
+++ pages/apac.js
@@ -1,3 +1,78 @@
-export default () => {
-  return <p>hi</p>
+import Error from 'next/error'
+import Grouping from '../components/grouping'
+import Regions from '../components/regions'
+import Meta from '@hackclub/meta'
+import { Box, Heading, Text, Link } from 'theme-ui'
+import Head from 'next/head'
+import Signup from '../components/signup'
+import Years from '../components/years'
+import { map, orderBy, find, kebabCase, startCase } from 'lodash'
+import { getGroupingData } from '../lib/data'
+
+export default ({ name, events, emailStats }) => {
+  const title = `High School Hackathons in ${new Date().getFullYear()}`
+
+  return (
+    <Grouping
+      includeMeta={false}
+      header={
+        <>
+          <Meta
+            as={Head}
+            title="High School Hackathons"
+            description={`${title}. A curated list of online and in-person high school hackathons in the Asia-Pacific region, maintained by Hack Club staff.`}
+          />
+          <Heading as="h1" variant="title" sx={{ color: 'primary' }}>
+            High School Hackathons in the Asia-Pacific Region
+          </Heading>
+          <Text as="p" variant="subtitle" sx={{ my: 3 }}>
+            A curated list of in-person and online high school hackathons.
+            <Text as="p" variant="subtitle">
+              {' '}
+              Maintained by the{' '}
+              <Link href="https://hackclub.com/">Hack Club</Link> staff.
+            </Text>
+          </Text>
+        </>
+      }
+      events={events}
+      footer={
+        <section>
+          {/* <Signup stats={emailStats} /> */}
+          <Heading variant="headline" sx={{ mt: [4, 5], mb: [3, 4] }}>
+            Explore by year
+          </Heading>
+          <Years />
+          <Heading variant="headline" sx={{ mt: [4, 5], mb: [3, 4] }}>
+            Explore popular regions
+          </Heading>
+          <Regions />
+        </section>
+      }
+    />
+  )
+}
+
+// const distance = (lat1, lon1, lat2, lon2) => {
+//   // https://www.geodatasource.com/developers/javascript
+//   const radlat1 = (Math.PI * lat1) / 180
+//   const radlat2 = (Math.PI * lat2) / 180
+//   const theta = lon1 - lon2
+//   const radtheta = (Math.PI * theta) / 180
+//   let dist =
+//     Math.sin(radlat1) * Math.sin(radlat2) +
+//     Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
+//   dist = Math.acos(dist)
+//   dist = (dist * 180) / Math.PI
+//   dist = dist * 60 * 1.1515
+//   return {
+//     miles: dist,
+//     kilometers: dist * 1.609344
+//   }
+// }
+
+export const getStaticProps = async () => {
+  let { events, emailStats } = await getGroupingData()
+  events = orderBy(events, 'start', 'desc')
+  return { props: { events, emailStats }, revalidate: 10 }
 }
