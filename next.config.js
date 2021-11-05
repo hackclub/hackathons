804a8a4bc6c930acec7ea7bf2280c77d4b5a3daa
eilla1 <git@ella.cx>
2021-11-05 13:53:35 -0700

    Add redirects to next config and use next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
-const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
-module.exports = withMDX({
+const nextConfig = {
   experimental: { trailingSlash: true },
-  pageExtensions: ['js', 'jsx', 'mdx']
-})
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  async redirects() {
+    return [
+      {
+        source: '/india',
+        destination: '/apac',
+        permanent: false
+      },
+      {
+        source: '/list-of-hackathons-in-asia-pacific',
+        destination: '/apac',
+        permanent: false
+      },
+      {
+        source: '/list-of-hackathons-in-india',
+        destination: '/apac',
+        permanent: false
+      }
+    ]
+  }
+}
+
+const withPlugins = require('next-compose-plugins')
+
+const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
+
+module.exports = withPlugins([withMDX], nextConfig)
