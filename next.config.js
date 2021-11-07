5e63c17a0c2d640977ad681e5f89e27e6bc5ae7d
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-11-07 11:15:50 +0800

    lol warned I have been

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const nextConfig = {
   experimental: { trailingSlash: true },
   pageExtensions: ['js', 'jsx', 'mdx'],
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true
   }
 }
