64df5f29d9608d78f30ef8ed7ac53b62eeda6591
eilla1 <git@ella.cx>
2021-11-06 19:29:28 -0700

    Ignore eslint during builds

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
-module.exports = withMDX({
+const nextConfig = {
   experimental: { trailingSlash: true },
-  pageExtensions: ['js', 'jsx', 'mdx']
-})
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  }
+}
+const withPlugins = require('next-compose-plugins')
+
+const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
+
+module.exports = withPlugins([withMDX], nextConfig)
