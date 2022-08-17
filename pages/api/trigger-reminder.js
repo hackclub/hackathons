6821e30ca34c806ac0572b68576b5c7d8a8e986d
Max Wofford <max@maxwofford.com>
2022-08-17 10:21:02 -0400

    Handle case token is not set

--- pages/api/trigger-reminder.js
+++ pages/api/trigger-reminder.js
@@ -1,5 +1,10 @@
 export default async (req, res) => {
-  const authed = req.headers['Authorization'] == 'Bearer ' + process.env.TOKEN
+  const token = process.env.TOKEN
+  if (!token) {
+    return res.status(200).json({msg: 'No token set, are you in dev/preview?'})
+  }
+
+  const authed = req.headers['Authorization'] == 'Bearer ' + token
 
   if (!authed) {
     return res.status(401).json({ error: 'Unauthorized' })
