b16da6629f8faeb797439b5b06416342ec7a0855
Max Wofford <max@maxwofford.com>
2022-08-17 13:25:52 -0400

    Add individual event trigger endpoints

---
+++ pages/api/events/[id]/remind.js
@@ -0,0 +1,15 @@
+export default async (req, res) => {
+  const token = process.env.TOKEN
+  if (!token) {
+    return res.status(200).json({msg: 'No token set, are you in dev/preview?'})
+  }
+
+  const authed = req.headers['Authorization'] == 'Bearer ' + token
+
+  if (!authed) {
+    return res.status(401).json({ error: 'Unauthorized' })
+  }
+
+  // gib email (for event)
+  const { id } = req.params
+}
