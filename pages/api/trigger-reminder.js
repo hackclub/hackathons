b314b77d4fd5c27c614dce7bc8eafc1c4818b599
Max Wofford <max@maxwofford.com>
2022-08-17 13:25:52 -0400

    Add individual event trigger endpoints

--- pages/api/trigger-reminder.js
+++ pages/api/trigger-reminder.js
@@ -1,3 +1,5 @@
+import { getEvents } from "../../lib/data"
+
 export default async (req, res) => {
   const token = process.env.TOKEN
   if (!token) {
@@ -10,6 +12,19 @@ export default async (req, res) => {
     return res.status(401).json({ error: 'Unauthorized' })
   }
 
-  // GIB EMAIL...
+  // get events that are upcoming and 2 weeks out
+  const events = await getEvents("AND(DATETIME_DIFF(start,TODAY(), 'days') < 14, DATETIME_DIFF(start,TODAY(), 'days') > 0)")
+
+  const eventPromises = events.map(event => 
+    fetch(`/api/events/${event.id}/remind`, {
+      method: 'POST',
+      headers: {
+        'Authorization': req.headers['Authorization']
+      }
+    })
+  )
+
+  await Promise.all(eventPromises)
+
   return res.status(200).json({msg: 'OK'})
 }
