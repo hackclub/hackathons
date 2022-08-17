1fd8bdd08cd7c9fa69d9b570386d1d9a97c2d95b
warren yun <wyun2006@gmail.com>
2022-08-16 22:09:42 -0400

    filter by virtual status

--- components/grouping.js
+++ components/grouping.js
@@ -1,23 +1,32 @@
 import { Container, Box, Grid } from 'theme-ui'
 import Header from '../components/header'
 import EventCard from '../components/event-card'
+import { useState } from 'react'
 
-export default ({ title, desc, header, children, footer, events }) => (
-  <Box
-    as="main"
-    sx={{ bg: 'background', color: 'text', textAlign: [null, 'center'] }}
-  >
-    <Header title={title} desc={desc} includeMeta>
-      {header}
-    </Header>
-    <Container sx={{ mt: [3, 4, 5] }}>
-      {children}
-      <Grid columns={[1, 2, 3]} gap={[3, 4]} sx={{ mt: [3, 4, 5] }}>
-        {events.map(event => (
-          <EventCard id={event.id} key={event.id} {...event} />
-        ))}
-      </Grid>
-      {footer}
-    </Container>
-  </Box>
-)
+export default ({ title, desc, header, children, footer, events }) => {
+  const [filter, setFilter] = useState(false);
+  const [virtual, setVirtual] = useState(false);
+
+  return (
+    <Box
+      as="main"
+      sx={{ bg: 'background', color: 'text', textAlign: [null, 'center'] }}
+    >
+      <Header title={title} desc={desc} includeMeta>
+        {header}
+      </Header>
+      <Container sx={{ mt: [3, 4, 5] }}>
+        {children}
+        <Container sx={{ mt: [2, 4, 5] }}></Container>
+        <Grid columns={[1, 2, 3]} gap={[3, 4]} sx={{ mt: [3, 4, 5] }}>
+          {events
+            .filter(event => event.virtual === true )
+            .map(event => (
+              <EventCard id={event.id} key={event.id} {...event} />
+            ))}
+        </Grid>
+        {footer}
+      </Container>
+    </Box>
+  )
+}
