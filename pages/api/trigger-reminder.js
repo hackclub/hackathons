849274a41c95951aa39453f5f1eea9460dc1a644
Ella <git@ella.cx>
2022-08-22 16:59:16 -0400

    Prettier

--- pages/api/trigger-reminder.js
+++ pages/api/trigger-reminder.js
@@ -1,9 +1,11 @@
-import { getEvents } from "../../lib/data"
+import { getEvents } from '../../lib/data'
 
 export default async (req, res) => {
   const token = process.env.TOKEN
   if (!token) {
-    return res.status(200).json({msg: 'No token set, are you in dev/preview?'})
+    return res
+      .status(200)
+      .json({ msg: 'No token set, are you in dev/preview?' })
   }
 
   const authed = req.headers['authorization'] == 'Bearer ' + token
@@ -13,18 +15,20 @@ export default async (req, res) => {
   }
 
   // get events that are upcoming and 2 weeks out
-  const events = await getEvents("AND(DATETIME_DIFF(start,TODAY(), 'days') < 14, DATETIME_DIFF(start,TODAY(), 'days') > 0)")
+  const events = await getEvents(
+    "AND(DATETIME_DIFF(start,TODAY(), 'days') < 14, DATETIME_DIFF(start,TODAY(), 'days') > 0)"
+  )
 
-  const eventPromises = events.map(event => 
+  const eventPromises = events.map(event =>
     fetch(`/api/events/${event.id}/remind`, {
       method: 'POST',
       headers: {
-        'authorization': req.headers['authorization']
+        authorization: req.headers['authorization']
       }
     })
   )
 
   await Promise.all(eventPromises)
 
-  return res.status(200).json({msg: 'OK'})
+  return res.status(200).json({ msg: 'OK' })
 }
