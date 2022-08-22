5f939d201e654929527c6862e29bca9a0edc8558
Ella <git@ella.cx>
2022-08-22 16:58:08 -0400

    Prettier

--- pages/api/subscribers/[id]/geocode.js
+++ pages/api/subscribers/[id]/geocode.js
@@ -1,9 +1,9 @@
 // given an "id" parameter, geocode the given airtable user and return {ok: true} if successful
-import {Client} from "@googlemaps/google-maps-services-js"
-import { getSubscriber, updateSubscriber } from "../../../../lib/data"
+import { Client } from '@googlemaps/google-maps-services-js'
+import { getSubscriber, updateSubscriber } from '../../../../lib/data'
 
 async function geocode(address) {
-  const client = new Client({key: process.env.GOOGLE_MAPS_API_KEY})
+  const client = new Client({ key: process.env.GOOGLE_MAPS_API_KEY })
   const { data } = await client.geocode({
     params: {
       address,
@@ -15,12 +15,24 @@ async function geocode(address) {
     latitude: result?.geometry?.location?.lat?.toString(),
     longitude: result?.geometry?.location?.lng?.toString(),
     parsed_address: result?.formatted_address,
-    parsed_city: result?.address_components?.find(c => c.types.includes('locality'))?.long_name,
-    parsed_state: result?.address_components?.find(c => c.types.includes('administrative_area_level_1'))?.long_name,
-    parsed_country: result?.address_components?.find(c => c.types.includes('country'))?.long_name,
-    parsed_state_code: result?.address_components?.find(c => c.types.includes('administrative_area_level_1'))?.short_name,
-    parsed_postal_code: result?.address_components?.find(c => c.types.includes('postal_code'))?.long_name?.toString(),
-    parsed_country_code: result?.address_components?.find(c => c.types.includes('country'))?.short_name,
+    parsed_city: result?.address_components?.find(c =>
+      c.types.includes('locality')
+    )?.long_name,
+    parsed_state: result?.address_components?.find(c =>
+      c.types.includes('administrative_area_level_1')
+    )?.long_name,
+    parsed_country: result?.address_components?.find(c =>
+      c.types.includes('country')
+    )?.long_name,
+    parsed_state_code: result?.address_components?.find(c =>
+      c.types.includes('administrative_area_level_1')
+    )?.short_name,
+    parsed_postal_code: result?.address_components
+      ?.find(c => c.types.includes('postal_code'))
+      ?.long_name?.toString(),
+    parsed_country_code: result?.address_components?.find(c =>
+      c.types.includes('country')
+    )?.short_name
   }
 }
 
@@ -44,7 +56,7 @@ export default async (req, res) => {
 
   const { location } = subscriber.fields
   const fields = await geocode(location)
-  
+
   const result = await updateSubscriber(id, fields)
-  res.json({ok: true, result})
+  res.json({ ok: true, result })
 }
