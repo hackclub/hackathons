66c57f3c8c8fe1253320ac344d4d77effdb8e8fb
Gary Tou <gary@garytou.com>
2023-08-18 18:13:31 -0700

    Revert "Use new Hackathons API (#268)" (#272)
    This reverts commit c72c2bcde31470d316dea1218007800ffa6f6392.

---
+++ pages/api/subscribers/unsubscribe.js
@@ -0,0 +1,21 @@
+import AirtablePlus from 'airtable-plus'
+
+const airtable = new AirtablePlus({
+  baseID: process.env.AIRTABLE_BASE_ID,
+  apiKey: process.env.AIRTABLE_API_KEY,
+  tableName:
+    process.env.VERCEL_ENV === 'production'
+      ? 'subscribers'
+      : 'subscribers_development'
+})
+
+export default async (req, res) => {
+  const { id } = req.query
+
+  const subscriber = await airtable.find(id)
+  airtable.updateWhere(`{id} = '${id}'`, {
+    unsubscribed_at: Date.now()
+  })
+
+  res.status(200).send(`Successfully unsubscribed ${subscriber.fields.email}`)
+}
